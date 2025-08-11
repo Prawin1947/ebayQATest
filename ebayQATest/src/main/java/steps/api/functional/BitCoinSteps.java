430110a29fd188dca43477b8fe24746438d169b2
Prawin <praveen.mayakar11@gmail.com>
2025-08-11 20:58:20 +0530

    QA-001: Added UI test

---
+++ ebayQATest/src/main/java/steps/api/functional/BitCoinSteps.java
@@ -0,0 +1,14 @@
+package steps.api.functional;
+
+import io.cucumber.java.en.Given;
+
+
+public class BitCoinSteps {
+
+    @Given("the user search the {string} item")
+    public void verifyBitCoinDetails(){
+
+    }
+
+
+}
---
+++ ebayQATest/src/test/java/features/api/functional/BitCoin.feature
@@ -0,0 +1,4 @@
+Feature: Verify the details of BitCoin
+
+  Scenario: Verify the BPI's from the response
+
